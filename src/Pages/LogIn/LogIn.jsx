7efb8b86033e9604ff743ce40d02dbfd708ef64e
jeandedieu <jeandedieusessou@gmail.com>
2024-07-29 09:42:14 +0000

    implement login page (use signup page style)

---
+++ src/Pages/LogIn/LogIn.jsx
@@ -0,0 +1,43 @@
+import React, { useState } from "react";
+import style from "../SignUp/SignUp.module.css";
+import CustomButton from "../../Components/CustomButton/CustomButton";
+import CustomInput from "../../Components/CustomInput/CustomInput";
+import { useNavigate } from "react-router-dom";
+
+const LogIn = () => {
+  const navigation = useNavigate();
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmitForm = (e, email, password) => {
+    e.preventDefault();
+    console.log({ email, password });
+  };
+  return (
+    <div className={style.body}>
+      <div className={style.container}>
+        <section>
+          <h3>Log In</h3>
+        </section>
+        <section>
+          <p>complete the form to log in</p>
+        </section>
+        <section className={style.form}>
+          <form onSubmit={(e) => handleSubmitForm(e, email, password)}>
+            <CustomInput placeholder={"Email"} setState={setEmail} />
+            <CustomInput placeholder={"Password"} setState={setPassword} />
+            <CustomButton>Log In</CustomButton>
+          </form>
+        </section>
+        <section>
+          <p>
+            haven't account ?{" "}
+            <span onClick={() => navigation("/signup")}>Sign Up</span>
+          </p>
+        </section>
+      </div>
+    </div>
+  );
+};
+
+export default LogIn;
