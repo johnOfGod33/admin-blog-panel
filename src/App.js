bd922d465708953d3d6d36e2e7c27550188c6717
jeandedieu <jeandedieusessou@gmail.com>
2024-07-29 09:43:22 +0000

    setup navigation between route with react router dom

--- src/App.js
+++ src/App.js
@@ -1,7 +1,17 @@
 import React from "react";
+import { BrowserRouter, Routes, Route } from "react-router-dom";
+import SignUp from "./Pages/SignUp/SignUp";
+import LogIn from "./Pages/LogIn/LogIn";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path="/login" Component={LogIn} />
+        <Route path="/signup" Component={SignUp} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
