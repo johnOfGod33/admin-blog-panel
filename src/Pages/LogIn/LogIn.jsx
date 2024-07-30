b6b9911207ac5aa4e3802af023996fc57734fc1f
jeandedieu <jeandedieusessou@gmail.com>
2024-07-30 08:37:13 +0000

    implement login user request

--- src/Pages/LogIn/LogIn.jsx
+++ src/Pages/LogIn/LogIn.jsx
@@ -3,15 +3,22 @@ import style from "../SignUp/SignUp.module.css";
 import CustomButton from "../../Components/CustomButton/CustomButton";
 import CustomInput from "../../Components/CustomInput/CustomInput";
 import { useNavigate } from "react-router-dom";
+import loginUser from "../../api/services/loginUser";
 
 const LogIn = () => {
   const navigation = useNavigate();
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
 
-  const handleSubmitForm = (e, email, password) => {
+  const handleSubmitForm = async (e, email, inputPassword) => {
     e.preventDefault();
-    console.log({ email, password });
+
+    try {
+      await loginUser(email, inputPassword);
+      navigation("/");
+    } catch (err) {
+      console.log(err);
+    }
   };
   return (
     <div className={style.body}>
---
+++ src/api/services/loginUser.js
@@ -0,0 +1,19 @@
+import { basicAxios } from "../config/axios";
+
+const loginUser = async (email, inputPassword) => {
+  try {
+    const response = await basicAxios.post("/users/login", {
+      email,
+      inputPassword,
+    });
+
+    console.log("user authentication sucess");
+    console.log({ accessToken: response.data });
+
+    return response;
+  } catch (err) {
+    throw err;
+  }
+};
+
+export default loginUser;
