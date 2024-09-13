194eb7c6c2eb5e110a53562dc9cc58d2c463fe52
jean de dieu <jeandedieusessou@gmail.com>
2024-09-13 13:45:19 +0000

    Revert "add header and menu layout component"
    This reverts commit ef0baef43ea6a744a110761c3756fca1057b1408.

--- src/App.js
+++ src/App.js
@@ -4,7 +4,6 @@ import SignUp from "./Pages/SignUp/SignUp";
 import LogIn from "./Pages/LogIn/LogIn";
 import ProtectedRoutes from "./Components/Layouts/ProtectedRoutes";
 import Articles from "./Pages/Articles/Articles";
-import MenuLayout from "./Components/Layouts/MenuLayout/MenuLayout";
 
 const App = () => {
   return (
@@ -13,9 +12,7 @@ const App = () => {
         <Route path="/login" Component={LogIn} />
         <Route path="/signup" Component={SignUp} />
         <Route path="/" Component={ProtectedRoutes}>
-          <Route Component={MenuLayout}>
-            <Route index Component={Articles} />
-          </Route>
+          <Route index Component={Articles} />
         </Route>
       </Routes>
     </BrowserRouter>
