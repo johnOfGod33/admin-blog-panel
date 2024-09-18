503dd60b54ae1d15d4325be36d477b8b1fdd4d23
jean de dieu <jeandedieusessou@gmail.com>
2024-09-18 13:43:25 +0000

    use editor js to create the  form

--- src/Pages/ArticleForm/ArticleForm.jsx
+++ src/Pages/ArticleForm/ArticleForm.jsx
@@ -1,8 +1,46 @@
-import React from "react";
+import React, { useEffect, useRef, useState } from "react";
 import style from "./ArticleForm.module.css";
 import { useParams } from "react-router-dom";
+import createEditor from "../../utils/createEditor";
+
 const ArticleForm = () => {
   const { action, articleId } = useParams();
+  const editorInstance = useRef(null);
+
+  useEffect(() => {
+    editorInstance.current = createEditor();
+  });
+
+  const button = () => {
+    if (action == "Create") {
+      return (
+        <>
+          <button className={style.form_buttons_publish} type="submit">
+            Publish
+          </button>
+          <button className={style.form_buttons_draft} type="submit">
+            Save Draft
+          </button>
+        </>
+      );
+    } else if (action == "Edit") {
+      return (
+        <>
+          <button className={style.form_buttons_save} type="submit">
+            Save Change
+          </button>
+        </>
+      );
+    }
+  };
+
+  const handleSubmitButton = (e) => {
+    e.preventDefault();
+
+    editorInstance.current.save().then((outputData) => {
+      console.log(outputData);
+    });
+  };
 
   return (
     <div className={style.container}>
@@ -10,15 +48,12 @@ const ArticleForm = () => {
         <h4> {action} article </h4>
       </section>
       <section className={style.form}>
-        <form>
-          <div>
-            <input type="text" placeholder="Article Title" />
+        <form onSubmit={(e) => handleSubmitButton(e)}>
+          <div className={style.form_title}>
+            <input type="text" placeholder="New article title here..." />
           </div>
-          <div className={style.form_buttons}>
-            <button>Publish</button>
-            <button>Save Draft</button>
-            <button>Save Change</button>
-          </div>
+          <div className={style.form_editor} id="editorjs"></div>;
+          <div className={style.form_buttons}>{button()}</div>
         </form>
       </section>
     </div>
